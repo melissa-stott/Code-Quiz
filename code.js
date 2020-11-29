7a05e91eb2f57c2a2c418a81662f32f0520e9f61
melissa-stott <1princessmelissa@gmail.com>
2020-11-29 10:57:06 -0800

    created button elements in HTML and wrote code for the timer function in JS.

--- code.js
+++ code.js
@@ -1,18 +1,32 @@
-var header = document.createElement("header");
-var center = document.createElement("h1");
-var theQuestions = document.createElement("div");
-var quest1 = document.createElement("p");
-var quest2 = document.createElement("p");
-var quest3 = document.createElement("p");
-var quest4 = document.createElement("p");
-var quest5 = document.createElement("p");
+var myButton = document.getElementById ("myButton");
+var decrButton = document.getElementById ("decrButton");
+var time = document.getElementById("time");
 
-document.body.append(header);
-document.body.appendChild(center);
-document.body.appendChild(theQuestions);
-document.body.appendChild(quest1);
+var secondsLeft = 30;
+var isRunning = false;
 
-center.textContent += 'Coding Quiz Challenge';
-center.style.padding = '50px';
-
-quest1.textContent += 'Javascript is ridiculously hard!'
+myButton.addEventListener("click", function () {
+    if(isRunning){
+        return;
+    } 
+    isRunning = true;
+    secondsLeft = 30;
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      time.textContent = secondsLeft + " remaining.";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+        }
+  
+    }, 1000);
+  })
+  function sendMessage() {
+    time.textContent = secondsLeft;
+    isRunning = false;
+  }
+  
+decrButton.addEventListener("click", function(){
+    secondsLeft -= 5;
+})
